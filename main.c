c849328d05d4798c92c0a1fc13d94b30477e590d
EimanEltayeb <em_moh092@hotmail.com>
2023-12-14 06:51:14 -0800

    main.c

--- main.c
+++ main.c
@@ -1,32 +1,85 @@
 #include "monty.h"
-stack_s **head;
+
+/**
+ * free_array - a function to free the line array
+ * @arr: the array to be freed
+ */
+void free_array(char **arr)
+{
+	int i;
+
+	for (i = 0; arr[i] != NULL; i++)
+		free(arr[i]);
+	free(arr);
+}
+
+
+/**
+ * psh2 - continue push function
+ * @n: data to be added to the list
+ * @head: pointer to the list
+ * Return: number indicating success or failure.
+ */
+int psh2(int n)
+{
+	stack_t *new;
+	stack_t *temp;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		return (-1);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (1);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	new->prev = temp;
+	return (1);
+}
 
 /**
  * psh - pushes element to stack
  * @arr: arguments array
+ * @line_num: line number in monty file
  * Return: 0 if error and 1 if success
  */
 
-int psh(char **arr)
+int psh(char **arr, int *line_num)
 {
-	int i, n;
+	int i = 0, n, check_push;
 
 	if (arr[1] == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", *line_num);
 		exit(EXIT_FAILURE);
 	}
-	while (arr[1][i] != NULL)
+	while (arr[1][i] != '\0')
 	{
-		if (arr[1][i] <= 48 || arr[1][i] >= 57)
+		if (arr[1][i] <= '0' || arr[1][i] >= '9')
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", *line_num);
+			free_array(arr);
 			exit(EXIT_FAILURE);
 		}
+		else
+			i++;
 	}
 	n = atoi(arr[1]);
-	
-
+	check_push = psh2(n);
+	free_array(arr);
+	return (check_push);
 }
 
 
@@ -38,40 +91,41 @@ int psh(char **arr)
  * @opcode: opcode
  * Return: number indicating the error 
  */
-int first_function(FILE *f, int *line_num, char *opcode)
+int first_function(FILE *f, int *line_num, char **opcode)
 {
-	char *line[256], *token, **arr;
+	char line[256], *token, **arr;
 	int i = 0;
 
 	*line_num = 1;
 	while (fgets(line, sizeof(line), f) != NULL)
 	{
-		arr = malloc(sizeof(line));
+		arr = malloc(sizeof(char *) * 2);
 		if (arr == NULL)
-			return (-1)
+			return (-1);
 		token = strtok(line, " \t\n");
-		while (token != NULL)
+		for (i = 0; i < 2; i++)
 		{
-			arr[i++] = strdup(token);
+			arr[i] = malloc(sizeof(char) * (strlen(token) + 1));
+			arr[i] = strcpy(arr[i], token);
 			token = strtok(NULL, " \t\n");
-			arr[i] = NULL;
-			if (i == 0)
-			{
-				free(arr);
-				return (-1);
-			}
+		}
+		arr[i] = NULL;
+		if (i == 0)
+		{
+			free(arr);
+			return (-1);
 		}
 		*opcode = arr[0];
 		if (strcmp(arr[0], "push") == 0)
-			psh(arr[1]);
+			psh(arr, line_num);
 
 
 
 
 
-		*line_num++;
+		(*line_num)++;
 	}
-
+	return (1);
 }
 
 /**
@@ -80,11 +134,12 @@ int first_function(FILE *f, int *line_num, char *opcode)
  * @argv: arguments
  * Return: 0
  */
+stack_t **head = NULL;
 int main(int argc, char *argv[])
 {
 	FILE *f;
-	char *name, *opcode;
-	int *line_num;
+	char *name = NULL, **opcode = NULL;
+	int *line_num = 0, check = 0;
        
 	if (argc != 2)
 	{
@@ -101,12 +156,12 @@ int main(int argc, char *argv[])
 	check = first_function(f, line_num, opcode);
 	if (check == 0)
 	{
-		fprint(stderr, "L%d: unknown instruction %s\n" *line_num, *opcode);
+		fprintf(stderr, "L%d: unknown instruction %s\n", *line_num, *opcode);
 		exit(EXIT_FAILURE);
 	}
 	if (check == -1)
 	{
-		fprint(stderr, "Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	fclose(f);
